4da8090693f8fa51673c1cdcdf130bf1420309c9
Sully <jackswilam@gmail.com>
2019-11-04 17:30:48 +0200

    Full CRUD operation for auctions

---
+++ DealDouble.Services/AuctionsService.cs
@@ -0,0 +1,53 @@
+﻿using DealDouble.Data;
+using DealDouble.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealDouble.Services
+{
+    public class AuctionsService
+    {
+        public List<Auction> GetAuctions()
+        {
+            var context = new DealDoubleContext();
+
+            return context.Auctions.ToList();
+        }
+
+        public Auction GetAuction(int id)
+        {
+            var context = new DealDoubleContext();
+
+            return context.Auctions.Where(a => a.Id == id).First();
+        }
+
+        public void SaveAuction(Auction auction)
+        {
+            var context = new DealDoubleContext();
+
+            context.Auctions.Add(auction);
+            context.SaveChanges();
+        }
+
+        public void UpdateAuction(Auction auctionModel)
+        {
+            var context = new DealDoubleContext();
+
+            context.Entry(auctionModel).State = System.Data.Entity.EntityState.Modified;
+
+            context.SaveChanges();
+        }
+
+        public void DeleteAuction(Auction auctionModel)
+        {
+            var context = new DealDoubleContext();
+
+            context.Entry(auctionModel).State = System.Data.Entity.EntityState.Deleted;
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DealDouble.Web/Controllers/AuctionsController.cs
@@ -0,0 +1,76 @@
+﻿using DealDouble.Entities;
+using DealDouble.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealDouble.Web.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var service = new AuctionsService();
+
+            var auctions  = service.GetAuctions();
+            
+            return View(auctions);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Auction auction)
+        {
+            var service = new AuctionsService();
+
+            service.SaveAuction(auction);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var service = new AuctionsService();
+
+            var auction = service.GetAuction(id);
+            return View(auction);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            var service = new AuctionsService();
+
+            service.UpdateAuction(auction);
+
+            return View(auction);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var service = new AuctionsService();
+
+            var auction = service.GetAuction(id);
+            return View(auction);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Auction auction)
+        {
+            var service = new AuctionsService();
+
+            service.DeleteAuction(auction);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
