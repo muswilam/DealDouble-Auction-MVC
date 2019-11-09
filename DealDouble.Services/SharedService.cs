05e7a720abe0d2f4f41db692acc03929ee8c3343
Sully <jackswilam@gmail.com>
2019-11-09 15:51:11 +0200

    Upload images, save them in pictures table in db, and display them again

---
+++ DealDouble.Services/SharedService.cs
@@ -0,0 +1,23 @@
+﻿using DealDouble.Data;
+using DealDouble.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealDouble.Services
+{
+    public class SharedService
+    {
+        public int SavePicture(Picture picture)
+        {
+            var context = new DealDoubleContext();
+
+            context.Pictures.Add(picture);
+            context.SaveChanges();
+
+            return picture.Id;
+        }
+    }
+}
---
+++ DealDouble.Web/Controllers/SharedController.cs
@@ -0,0 +1,50 @@
+﻿using DealDouble.Entities;
+using DealDouble.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealDouble.Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        SharedService service = new SharedService();
+
+        [HttpPost]
+        public JsonResult UploadPictures()
+        {
+            JsonResult json = new JsonResult();
+
+            var picturesJSON = new List<object>();
+
+            var pictures = Request.Files;
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                var picture = pictures[i];
+
+                //create picName, and path(url) 
+                var fileName = Guid.NewGuid() + Path.GetExtension(picture.FileName);
+                var path = Server.MapPath("~/Content/images/") + fileName;
+                
+                //upload pic into images file
+                picture.SaveAs(path);
+
+                //save pic url in db
+                var newPic = new Picture();
+                newPic.Url = path;
+
+                int picId = service.SavePicture(newPic);
+
+                picturesJSON.Add(new { id = picId, url = "/Content/images/" + fileName });
+            }
+
+            json.Data = picturesJSON;
+
+            return json;
+        }
+    }
+}
